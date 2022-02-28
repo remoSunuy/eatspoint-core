2430147364e828e2b720c7eb8a4b077d25f31e91
Yunus Oncel <yunusoncel@gmail.com>
2022-02-28 21:12:51 +0300

    remove ip

--- dto/register_dto.go
+++ dto/register_dto.go
@@ -2,7 +2,6 @@ package dto
 
 type ServiceRegisterDTO struct {
 	Name				string				`json:"name"`
-	IP					string				`json:"ip"`
 	Port				int					`json:"port"`
 	HealthCheckDTO		HealthCheckDTO		`json:"healthCheck"`
 }
